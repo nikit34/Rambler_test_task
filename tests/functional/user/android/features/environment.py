1ba18778086c25d99d1282133d5c569e72e8a390
Никита Пермяков <permikov134@mail.ru>
2020-11-29 00:11:31 +0500

    fix version android

--- tests/functional/user/android/features/environment.py
+++ tests/functional/user/android/features/environment.py
@@ -5,11 +5,10 @@ from app.application import Application
 def before_senario(context, scenario):
     desired_cap = {
       "platformName": "Android",
-      "platformVersion": "11",
       "deviceName": "Android Emulator",
       "app": "C:\\Users\\permi\\source\\repos\\draft\\3\\tests\\functional\\user\\android\\src\\app.apk",
       "appPackage": "ru.rambler.kassa",
-      # "appWaitActivity": "ru.rambler.popcorn.sdk.presentation.screens.onboarding.OnBoardingActivity"
+      "appWaitActivity": "ru.rambler.popcorn.sdk.presentation.screens.onboarding.OnBoardingActivity"
     }
     context.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=desired_cap)
     context.driver.implicitly_wait(5)
