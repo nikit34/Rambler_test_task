102221071e57cbf83192670d5c73e5cecfab552c
Никита Пермяков <permikov134@mail.ru>
2020-11-28 19:44:54 +0500

    template for search page - need refactoring for stricter typing

---
+++ tests/functional/user/android/pages/search_page.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from .base_page import Page
+
+
+class SearchPage(Page):
+    pass
+
+    def input_search(self, search_phrase: str):
+        self.input(search_phrase, '<*locator>')
+
+    def verify_search_result(self, search_phrase: str):
+        result_text = self.find_element('<*locator>').text
+        assert search_phrase in result_text, f'Expected {search_phrase} be in {result_text}'
