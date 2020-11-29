a011d406d10f25eeb1acbd455e51d0b5f0db9c96
Никита Пермяков <permikov134@mail.ru>
2020-11-29 10:30:55 +0500

    refactoring popular_page.py and add selectsfor onboarding_page.py

--- tests/functional/user/android/pages/onboarding_page.py
+++ tests/functional/user/android/pages/onboarding_page.py
@@ -3,4 +3,9 @@ from .base_page import Page
 
 
 class OnboardingPage(Page):
-    pass
+    def __init__(self):
+        self.TITLE_TEXT = (By.ID, 'ru.rambler.kassa:id/text_title')
+        self.MAIN_TEXT = (By.ID, 'ru.rambler.kassa:id/text_message')
+
+        self.CLOSE_BTN = (By.ID, 'ru.rambler.kassa:id/button_close')
+        self.NEXT_BTN = (By.ID, 'ru.rambler.kassa:id/button_next')
--- tests/functional/user/android/pages/popular_page.py
+++ tests/functional/user/android/pages/popular_page.py
@@ -3,18 +3,8 @@ from .base_page import Page
 
 
 class PopularPage(Page):
-    ONBOARDING_NEXT_BTN = (By.ID, 'ru.rambler.kassa:id/button_next')
-    ONBOARDING_CANCEL_BTN = (By.ID, 'ru.rambler.kassa:id/button_close')
+    def __init__(self):
+        self.MAIN_MOVE = (By.ID, '')
 
-    def tap_popular(self):
-        self.click(self.POPULAR_FOOTER_BTN)
 
-    def tap_category(self):
-        self.click(self.CATEGORY_FOOTER_BTN)
-
-    def tap_search(self):
-        self.click(self.SEARCH_FOOTER_BTN)
-
-    def tap_profile(self):
-        self.click(self.PROFILE_FOOTER_BTN)
 
