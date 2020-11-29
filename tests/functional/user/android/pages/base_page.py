4d0bb2156d2d6418960c856f57a5e405c33b4569
Никита Пермяков <permikov134@mail.ru>
2020-11-29 08:04:18 +0500

    add search with modes

--- tests/functional/user/android/pages/base_page.py
+++ tests/functional/user/android/pages/base_page.py
@@ -13,3 +13,34 @@ class Page:
         e = self.find_element(*locator)
         e.clear()
         e.send_key(text)
+
+
+class Search:
+    def __init__(self, capacity=100, size_windows=50, *args, **kwargs):
+        self.capacity = capacity
+        self.size_windows = size_windows
+
+    @staticmethod
+    def matching_text(token: str, pattern: str):
+        if '*' in token:
+            return Search.partially_matches(token, pattern)
+        return Search.completely_matches(token, pattern)
+
+    @classmethod
+    def partially_matches(cls, token: str, pattern: str):
+        separate_token = token.split('*')
+        current_pattern = pattern
+        found_result = []
+        for part_token in separate_token:
+            found_index = current_pattern.find(part_token)
+            if found_index == -1:
+                continue
+            current_pattern = current_pattern[found_index:]
+            found_result.append(part_token)
+        return found_result
+
+    @classmethod
+    def completely_matches(cls, token: str, pattern: str):
+        if pattern.find(token) == -1:
+            return False
+        return True
