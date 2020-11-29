ce9849639d08ab3d5091eaf70a81191ad0569102
Никита Пермяков <permikov134@mail.ru>
2020-11-29 08:11:58 +0500

    init page to context

--- tests/functional/user/android/app/application.py
+++ tests/functional/user/android/app/application.py
@@ -1,8 +1,14 @@
+from pages.base_page import Page
 from pages.main_page import MainPage
+from pages.category_page import CategoryPage
+from pages.search_page import SearchPage
 from pages.profile_page import ProfilePage
 
 
 class Application:
     def __init__(self, driver):
+        self.base_page = Page(driver)
         self.main_page = MainPage(driver)
+        self.category_page = CategoryPage(driver)
+        self.search_page = SearchPage(driver)
         self.profile_page = ProfilePage(driver)
