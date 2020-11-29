6621c4ba318bfeb82240249a2c1e96c09111709a
Никита Пермяков <permikov134@mail.ru>
2020-11-29 20:16:25 +0500

    delete trash

--- tests/functional/user/android/features/steps/search.py
+++ tests/functional/user/android/features/steps/search.py
@@ -1,21 +1,6 @@
 from behave import given, when, then
 
 
-@given('see button {object:S} is "{state}" state and "{ready}" ready to press on {base:S}')
-def step_impl(context, object, state, ready, base):
-    pass
-
-
-@when('see button {object:S} is "{state}" state and "{ready}" ready to press on {base:S}')
-def step_impl(context, object, state, ready, base):
-    pass
-
-
-@then('see button {object:S} is "{state}" state and "{ready}" ready to press on {base:S}')
-def step_impl(context, object, state, ready, base):
-    pass
-
-
 @given('see match text "{text}" and {object:S} on {base:S}')
 def step_impl(context, text, object, base):
     pass
@@ -46,11 +31,6 @@ def step_impl(context, object, text, base):
 
 @when('see button {object:S} with "{text}" text on {base:S}')
 def step_impl(context, object, text, base):
-    pass
-
-
-@then('see button {object:S} with "{text}" text on {base:S}')
-def step_impl(context, object, text, base):
     if base == 'ONBOARDING_PAGE':
         if object == 'NEXT_BTN':
             context.app.onboarding_page.verify_next_btn(text)
@@ -62,34 +42,12 @@ def step_impl(context, object, text, base):
         raise KeyError(f'{base} is not defined')
 
 
-@given('see button {object:S} is "{ready}" ready to press on {base:S}')
-def step_impl(context, object, ready, base):
+@then('see button {object:S} with "{text}" text on {base:S}')
+def step_impl(context, object, text, base):
     pass
 
 
-@when('see button {object:S} is "{ready}" ready to press on {base:S}')
-def step_impl(context, object, ready, base):
-    pass
 
-
-@then('see button {object:S} is "{ready}" ready to press on {base:S}')
-def step_impl(context, object, ready, base):
-    pass
-
-
-@given('see button {object:S} is "{state}" state on {base:S}')
-def step_impl(context, object, state, base):
-    pass
-
-
-@when('see button {object:S} is "{state}" state on {base:S}')
-def step_impl(context, object, state, base):
-    pass
-
-
-@then('see button {object:S} is "{state}" state on {base:S}')
-def step_impl(context, object, state, base):
-    pass
 
 
 
--- tests/functional/user/android/features/tests/search.feature
+++ tests/functional/user/android/features/tests/search.feature
@@ -6,8 +6,8 @@ Feature: Check Onboarding page by template
   Scenario: Check text by matches and buttons
     When see match text "*покажите смартфон* *проходите сразу в зал*" and MAIN_TEXT on ONBOARDING_PAGE
     And see match text "Живой билет" and TITLE_TEXT on ONBOARDING_PAGE
-    Then see button CLOSE_BTN with "ЗАКРЫТЬ" text on ONBOARDING_PAGE
     And see button NEXT_BTN with "ДАЛЕЕ" text on ONBOARDING_PAGE
+    And see button CLOSE_BTN with "ЗАКРЫТЬ" text on ONBOARDING_PAGE
 
 
 
