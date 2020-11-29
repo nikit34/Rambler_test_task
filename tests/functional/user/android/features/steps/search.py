507cfb27a6b86bc0ea5474079bef77191c4f5be9
Никита Пермяков <permikov134@mail.ru>
2020-11-29 18:46:43 +0500

    add check page and state of objects

--- tests/functional/user/android/features/steps/search.py
+++ tests/functional/user/android/features/steps/search.py
@@ -1,98 +1,94 @@
 from behave import given, when, then
 
 
-@given('see button {object:S} is "{state}" state and "{ready}" ready to press')
-def step_impl(context, object, state, ready):
+@given('see button {object:S} is "{state}" state and "{ready}" ready to press on {base:S}')
+def step_impl(context, object, state, ready, base):
     pass
 
 
-@when('see button {object:S} is "{state}" state and "{ready}" ready to press')
-def step_impl(context, object, state, ready):
+@when('see button {object:S} is "{state}" state and "{ready}" ready to press on {base:S}')
+def step_impl(context, object, state, ready, base):
     pass
 
 
-@then('see button {object:S} is "{state}" state and "{ready}" ready to press')
-def step_impl(context, object, state, ready):
+@then('see button {object:S} is "{state}" state and "{ready}" ready to press on {base:S}')
+def step_impl(context, object, state, ready, base):
     pass
 
 
-@given('see partially matches text "{text}" and {object:S}')
-def step_impl(context, text, object):
-    if object == 'TITLE_TEXT':
-        context.app.onboarding_page.verify_title_text(text)
-    elif object == 'MAIN_TEXT':
-        context.app.onboarding_page.verify_main_text(text)
+@given('see "{mode}" matches text "{text}" and {object:S} on {base:S}')
+def step_impl(context, mode, text, object, base):
+    pass
+
+
+@when('see "{mode}" matches text "{text}" and {object:S} on {base:S}')
+def step_impl(context, mode, text, object, base):
+    if base == 'ONBOARDING_PAGE':
+        if mode == 'completely' and object == 'TITLE_TEXT':
+            context.app.onboarding_page.verify_title_text(text)
+        elif mode == 'partially' and object == 'MAIN_TEXT':
+            context.app.onboarding_page.verify_main_text(text)
+        else:
+            raise KeyError(f'{mode} or {object} is not defined')
     else:
-        raise KeyError('<object> is not defined')
+        raise KeyError(f'{base} is not defined')
 
 
-@when('see partially matches text "{text}" and {object:S}')
-def step_impl(context, text, object):
+@then('see "{mode}" matches text "{text}" and {object:S} on {base:S}')
+def step_impl(context, mode, text, object, base):
     pass
 
 
-@then('see partially matches text "{text}" and {object:S}')
-def step_impl(context, text, object):
+@given('see button {object:S} with "{text}" text on {base:S}')
+def step_impl(context, object, text, base):
     pass
 
 
-@given('see completely matches text "{text}" and {object:S}')
-def step_impl(context, text, object):
+@when('see button {object:S} with "{text}" text on {base:S}')
+def step_impl(context, object, text, base):
     pass
 
 
-@when('see completely matches text "{text}" and {object:S}')
-def step_impl(context, text, object):
+@then('see button {object:S} with "{text}" text on {base:S}')
+def step_impl(context, object, text, base):
+    if base == 'ONBOARDING_PAGE':
+        if object == 'NEXT_BTN':
+            pass
+        elif object == 'CLOSE_BTN':
+            pass
+        else:
+            raise KeyError(f'{object} is not defined')
+    else:
+        raise KeyError(f'{base} is not defined')
+
+
+@given('see button {object:S} is "{ready}" ready to press on {base:S}')
+def step_impl(context, object, ready, base):
     pass
 
 
-@then('see completely matches text "{text}" and {object:S}')
-def step_impl(context, text, object):
+@when('see button {object:S} is "{ready}" ready to press on {base:S}')
+def step_impl(context, object, ready, base):
     pass
 
 
-@given('see button {object:S} with "{text}" text')
-def step_impl(context, object, text):
+@then('see button {object:S} is "{ready}" ready to press on {base:S}')
+def step_impl(context, object, ready, base):
     pass
 
 
-@when('see button {object:S} with "{text}" text')
-def step_impl(context, object, text):
+@given('see button {object:S} is "{state}" state on {base:S}')
+def step_impl(context, object, state, base):
     pass
 
 
-@then('see button {object:S} with "{text}" text')
-def step_impl(context, object, text):
+@when('see button {object:S} is "{state}" state on {base:S}')
+def step_impl(context, object, state, base):
     pass
 
 
-@given('see button {object:S} is "{ready}" ready to press')
-def step_impl(context, object, ready):
-    pass
-
-
-@when('see button {object:S} is "{ready}" ready to press')
-def step_impl(context, object, ready):
-    pass
-
-
-@then('see button {object:S} is "{ready}" ready to press')
-def step_impl(context, object, ready):
-    pass
-
-
-@given('see button {object:S} is "{state}" state')
-def step_impl(context, object, state):
-    pass
-
-
-@when('see button {object:S} is "{state}" state')
-def step_impl(context, object, state):
-    pass
-
-
-@then('see button {object:S} is "{state}" state')
-def step_impl(context, object, state):
+@then('see button {object:S} is "{state}" state on {base:S}')
+def step_impl(context, object, state, base):
     pass
 
 
